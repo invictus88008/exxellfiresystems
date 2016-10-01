d5fa99e9688a368bfe3808ca59eb1d1294ef5ee1
Nate Hargitt <Invictus88008@yahoo.com>
2016-09-30 17:04:29 -0700

    adding all changes.  Many changes have occured such as seo changes, added images etc.

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -14,10 +14,14 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
+$recipients = array(
+   "nathanhargitt@gmail.com",
+   "exxellfiresystems@gmail.com"
+   );
 	
-// create email body and send it	
-$to = 'your-email@your-domain.com'; // PUT YOUR EMAIL ADDRESS HERE
-$email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+
+$to = 'exxellfiresystems@gmail.com'; 
+$email_subject = "Message from:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@your-domain.com\n";
 $headers .= "Reply-To: $email_address";	
---
+++ js/cta-jump-in.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$(.cta).animate(
+		{display:block},
+		700,
+		easeInOutElasitc,
+		)
+});
